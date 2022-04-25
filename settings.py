5118b1980197195f22b439d593f5f0be3b2077fa
Robert Shields <you@example.com>
2022-04-25 16:33:31 -0700

    first commit

---
+++ settings.py
@@ -0,0 +1,17 @@
+class Settings:
+    #a class to store all settings for Alien invasion"
+
+    def __init__(self):
+        #initialize game settings
+
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230,230,230)
+        self.ship_speed = 1.5
+
+            #bullet settings
+        self.bullet_speed = 1.0
+        self.bullet_width = 4
+        self.bullet_height = 15
+        self.bullet_color = (60,60,60)
+        self.bullets_allowed = 5
