8e8ac94c4db7824cb280c5e3a207fb28c15f213a
Robert Shields <you@example.com>
2022-12-05 18:31:10 -0800

    slowed down speed

--- settings.py
+++ settings.py
@@ -7,7 +7,7 @@ class Settings:
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230,230,230)
-        self.ship_speed = 1.5
+        self.ship_speed = 1.0
         self.ship_limit = 3
 
             #bullet settings
